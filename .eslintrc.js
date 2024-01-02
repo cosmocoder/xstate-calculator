5442ceefbb9e244943b66aa1299841c49c578e04
Nilok Bose <cosmocoder@gmail.com>
2024-01-02 16:31:37 +0530

    chore: add eslint-plugin-xstate

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:xstate/all',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
